318a2312a4beff8a7906e136cd56f950d63ebc23
LuisArmandoOlmos <luisarmandoolmosolmos@gmail.com>
2022-11-16 16:57:51 -0400

    Add HeroPage.jsx and SearchPage.jsx

--- src/assets/routers/AppRouter.jsx
+++ src/assets/routers/AppRouter.jsx
@@ -1,15 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { DcPage, LoginPage, MarvelPage } from "../pages";
+import { Navbar } from "../components/Navbar";
+import { DcPage, HeroPage, LoginPage, MarvelPage, SearchPage } from "../pages";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path="dc" element={<DcPage />} />
         <Route path="marvel" element={<MarvelPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="hero" element={<HeroPage />} />
         <Route path="login" element={<LoginPage />} />
 
-        <Route path="/" element={<Navigate to="/dc" />} />
+        <Route path="/" element={<Navigate to="/marvel" />} />
       </Routes>
     </>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      black: "#151515",
+      gray: "#202020",
+      primary: "#EC1D24",
+    },
   },
   plugins: [],
 };
